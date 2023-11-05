780bedf943f137b971127175813c219ec789bdbc
Goah5 <zogar7777@gmail.com>
2023-11-05 16:51:18 +0600

    25

--- temp25.py
+++ temp25.py
@@ -1,30 +1,38 @@
 from random import randint
 
 
-def main(inp):
-	# NdX±C
+def roll(inp):
+    # NdX±C
 
-	# inp = input().lower()
-	# inp = "d20"
+    # inp = input().lower()
+    # inp = "d20"
 
-	n, c, = 1, 0
-	d_index, zc_index = inp.find("d"), -1
+    n, c, = 1, 0
+    d_index, zc_index = inp.find("d"), -1
 
-	for i in ("+", "-"):
-		if i in inp:
-			zc_index = inp.find(i)
-			c = int(inp[zc_index:])
+    for i in ("+", "-"):
+        if i in inp:
+            zc_index = inp.find(i)
+            c = int(inp[zc_index:])
 
-	n = int(inp[:d_index]) if d_index != 0 else 1
-	x = int(inp[d_index+1:zc_index] if zc_index != -1 else inp[d_index+1:])
+    n = int(inp[:d_index]) if d_index != 0 else 1
+    x = int(inp[d_index+1:zc_index] if zc_index != -1 else inp[d_index+1:])
+    if (d_index, zc_index) == (0,0):
+        x = int(inp)
+        c = 0
+        n = 1
 
-	# print(n, x, c)
-	
-	return [randint(1,x)+c for _ in range(n)] 
-	
+    print(n, x, c)
+    print(d_index, zc_index)
+
+    return [randint(1, x)+c for _ in range(n)]
 
 
 if __name__ == "__main__":
-	for i in ("d12", "4d5+2", "d7-2", "2 d10"):
-		print(i, "\n", *main(i))
-		
+    inp = 0
+    while not inp == "":
+        inp = input().lower()
+        print(*roll(inp))
+
+    for i in ("d12", "4d5+2", "d7-2", "2 d10"):
+        print(i, "\n", *roll(i))
